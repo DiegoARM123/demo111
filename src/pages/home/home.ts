d22aef5a6e9e2d68e4cc31169d0b1ee34bf6423c
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-04-19 15:47:48 -0400

    demo work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ LatLng,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -8,7 +19,59 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public geolocation: Geolocation,
+    public googleMaps: GoogleMaps
+  ) {}
+
+  ionViewDidLoad(){
+    this.obtenerPosicion();
+  }
+
+  obtenerPosicion():any{
+    this.geolocation.getCurrentPosition().then(response => {
+      this.loadMap(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }
+
+  loadMap(postion: Geoposition){
+    let latitude = postion.coords.latitude;
+    let longitud = postion.coords.longitude;
+    console.log(latitude, longitud);
+   
+    // create a new map by passing HTMLElement
+    let element: HTMLElement = document.getElementById('map');
+
+    let map: GoogleMap = this.googleMaps.create(element);
+
+    // create LatLng object
+    let myPosition: LatLng = new LatLng(latitude,longitud);
+
+    // create CameraPosition
+    let position: CameraPosition = {
+      target: myPosition,
+      zoom: 18,
+      tilt: 30
+    };
+
+    map.one(GoogleMapsEvent.MAP_READY).then(()=>{
+      console.log('Map is ready!');
+      
+      // move the map's camera to position
+      map.moveCamera(position);
+
+      // create new marker
+      let markerOptions: MarkerOptions = {
+        position: myPosition,
+        title: 'Here'
+      };
+      map.addMarker(markerOptions);
+    });
+
 
   }
 
